c635c6569f1cc83072daac02e272e9098800d4ef
alex <smartmanage715@gmail.com>
2022-11-04 23:20:16 +0900

    set API_URL in config

--- vue-3-frontend/src/services/auth.service.js
+++ vue-3-frontend/src/services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
+import { config } from "../config";
 import authHeader from "./auth-header";
-
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = config.API_URL;
 export default {
   login(user) {
     return axios
