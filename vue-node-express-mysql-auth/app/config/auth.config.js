7e1218992d5171330378499d4fb0c8727c8c3fad
alex <smartmanage715@gmail.com>
2022-11-04 23:53:09 +0900

    set frontend_url

--- vue-node-express-mysql-auth/app/config/auth.config.js
+++ vue-node-express-mysql-auth/app/config/auth.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   secret: "alex-secret-key",
+  FRONTEND_URL: "http://localhost:8081",
   APP_KEY: "base64:test",
   MAIL_USERNAME: "54aef5ba4c993a",
   MAIL_PASSWORD: "34bb4dfa94daef"
