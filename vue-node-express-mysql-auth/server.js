24b4c8dd9318751c38c13fad6e91a4e32ef50470
alex <smartmanage715@gmail.com>
2022-11-05 01:28:37 +0900

    cors issue

--- vue-node-express-mysql-auth/server.js
+++ vue-node-express-mysql-auth/server.js
@@ -2,10 +2,26 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const config = require("./app/config/auth.config");
-var corsOptions = {
-  origin: config.FRONTEND_URL,
-};
-app.use(cors(corsOptions));
+// var corsOptions = {
+//   origin: config.FRONTEND_URL,
+// };
+// app.use(cors(corsOptions));
+
+var allowedOrigins = [config.FRONTEND_URL];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 // parse requests of content-type - application/json
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
